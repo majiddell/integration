24fe077e3d5e8b46bda204e7e493becc1d0b0a5c
majiddell <48320237+majiddell@users.noreply.github.com>
2020-07-20 15:20:35 +0430

    Update se_project.java

--- se_project.java
+++ se_project.java
@@ -5,7 +5,7 @@ public class se_project {
 	public static void main(String[] args) {
 		
 		for (int i = 0; i < 5; i++) {
-			System.out.println("first version");
+			System.out.println("first version _ 2");
 		}
 		
 	}
